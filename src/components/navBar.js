826079b0ea021c615b10db0a58e71bee38baca69
Fredo St Fleur <fredostfleur23@gmail.com>
2023-05-15 16:50:28 -0400

    - Add navbar component -Choose one logo -Add and Comment future nav items

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navBar">
     <div className="navItems">
       <div id="logo">
         <div className="logoImage" />
-        <div> Space Travelers&apos;Hub</div>
+        <div className="logoText"> Space Travelers&apos;Hub</div>
       </div>
-      <ul className="navLinks">
+      {/* <ul className="navLinks">
         <li><Link to="/">Rockets</Link></li>
         <li><Link to="mission">Missions</Link></li>
         <li><Link to="myprofile">| My Profile</Link></li>
-      </ul>
+      </ul> */}
     </div>
   </nav>
 );
