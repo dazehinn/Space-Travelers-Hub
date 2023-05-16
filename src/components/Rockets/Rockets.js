f787a3bf981e9d69d6c6fc08e319dce4fc876ee6
dazehinn <dazehinn@yahoo.fr>
2023-05-16 22:47:57 +0100

    Render rocket info from API

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,24 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import styles from './Rockets.module.css';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
 const Rockets = () => {
-  const rocket = {
-    id: 1,
-    flickr_images: 'https://imgur.com/DaCfMsj.jpg',
-    name: 'Falcon 1',
-    description: 'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
-  };
+  const { rocketArray, isLoading } = useSelector((store) => store.rocket);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <div className={styles.rocketCard}>
-        <img className={styles.rocketImg} src={rocket.flickr_images} alt="rocket" />
-        <div className={styles.rocketInfo}>
-          <h2>{rocket.name}</h2>
-          <p>{rocket.description}</p>
-          <button type="button">Reserve Rocket</button>
+    rocketArray.map((rocket) => (
+      <div key={rocket.id}>
+        <div className={styles.rocketCard}>
+          <img className={styles.rocketImg} src={rocket.flickr_images} alt="rocket" />
+          <div className={styles.rocketInfo}>
+            <h2>{rocket.name}</h2>
+            <p>{rocket.description}</p>
+            <button className={styles.reserveBtn} type="button">Reserve Rocket</button>
+          </div>
         </div>
       </div>
-    </div>
+    ))
   );
 };
 
