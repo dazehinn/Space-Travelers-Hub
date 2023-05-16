d2ea5ac1a8cceeb3c5b6afe5bbfddd43492b3dcd
Fredo St Fleur <fredostfleur23@gmail.com>
2023-05-16 12:00:10 -0400

    -Add missions path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navBar';
 import Profiles from './components/profiles';
+import Missions from './components/missions';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route exact path="myprofile" element={<Profiles />} />
+        <Route exact path="mission" element={<Missions />} />
       </Routes>
     </div>
   );
