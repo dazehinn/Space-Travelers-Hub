793556be731a430926712a69c914d95e7d6a4916
dazehinn <dazehinn@yahoo.fr>
2023-05-18 20:24:59 +0100

    Add cancel buttons and invoke reducers

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import styles from './Rockets.module.css';
-import { fetchRockets } from '../../redux/rockets/rocketsSlice';
+import { fetchRockets, reserveRocket, cancelRocket } from '../../redux/rockets/rocketsSlice';
 
 const Rockets = () => {
   const { rocketArray, isLoading } = useSelector((store) => store.rocket);
@@ -22,13 +22,36 @@ const Rockets = () => {
 
   return (
     rocketArray.map((rocket) => (
+
       <div key={rocket.id}>
         <div className={styles.rocketCard}>
           <img className={styles.rocketImg} src={rocket.flickr_images} alt="rocket" />
           <div className={styles.rocketInfo}>
             <h2>{rocket.name}</h2>
-            <p>{rocket.description}</p>
-            <button className={styles.reserveBtn} type="button">Reserve Rocket</button>
+            <p>
+              <span className={rocket.reserved ? styles.reserved : styles.noReserved}>
+                Reserved
+              </span>
+              {rocket.description}
+            </p>
+            <button
+              className={rocket.reserved ? styles.noReserved : styles.reserveBtn}
+              type="button"
+              onClick={() => (
+                dispatch(reserveRocket(rocket.id)))}
+            >
+              Reserve Rocket
+            </button>
+
+            <button
+              className={rocket.reserved ? styles.cancelBtn : styles.noReserved}
+              type="button"
+              onClick={() => (
+                dispatch(cancelRocket(rocket.id)))}
+            >
+              Cancel Reservation
+            </button>
+
           </div>
         </div>
       </div>
