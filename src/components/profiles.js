6380c0fb1980737841d23e75980f176267e5a990
Fredo St Fleur <fredostfleur23@gmail.com>
2023-05-19 21:24:36 -0400

    -Import active mission -render mission joined -Merge rocket profil with mission profil

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -1,20 +1,46 @@
-import { useSelector } from 'react-redux';
-import '../App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/missionSlice';
 
 const Profiles = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(getMissions());
+    }
+  }, [dispatch, missions.length]);
+
+  const filteredMission = missions.filter((mission) => mission.joined === true);
   const { rocketArray } = useSelector((store) => store.rocket);
 
-  {
-    const reservedRockets = rocketArray.filter((rocket) => rocket.isReserved === true);
-    return (
+  const reservedRockets = rocketArray.filter((rocket) => rocket.isReserved === true);
+
+  return (
+    <div className="profileB">
       <div className="rocketProfile">
-        <p> Profile Coming Soon ...</p>
+        <h2>My Missions</h2>
+        {filteredMission.length > 0 ? (
+          <div className="border rounded">
+            {missions.filter((mission) => mission.joined).map((mission) => (
+              <div key={mission.mission_id}>
+                <h3 className="reservedRocketProfile">{mission.mission_name}</h3>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Mission not joined yet</p>
+        )}
+      </div>
+
+      <div className="rocketProfile">
         <h2>My Rockets</h2>
         {reservedRockets.map((rocket) => (
           <h3 key={rocket.id} className="reservedRocketProfile">{rocket.name}</h3>))}
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Profiles;
